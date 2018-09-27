cf3c775dc3adaed345ca763378959a249a6f064c
Koushik Das <koushik355@gmail.com>
2018-09-27 11:37:12 +0530

    working on rotation

--- src/Deck.js
+++ src/Deck.js
@@ -4,7 +4,7 @@ import {View, Animated, PanResponder} from 'react-native';
 class Deck extends React.Component {
     constructor(props) {
         super(props);
-        const position = new Animated.ValueXY();
+        const position = new Animated.ValueXY(); // value of position of animated component
         const panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: (event, gesture) => {
@@ -19,19 +19,39 @@ class Deck extends React.Component {
         this.state = {panResponder, position};
     }
 
+    getCardStyle() {
+        const {position} = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-500, 0, 500],
+            outputRange: ['-120 deg', '0 deg', '120 deg']
+        });
+        return {
+            ...position.getLayout(),
+            transform: [{rotate}]
+        };
+    }
+
     renderCards() {
-        return this.props.data.map(item => {
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={this.getCardStyle()}
+                        {...this.state.panResponder.panHandlers}>
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )
+            }
             return this.props.renderCard(item);
         });
     }
 
     render() {
         return (
-            <Animated.View
-                style={this.state.position.getLayout()}
-                {...this.state.panResponder.panHandlers}>
+            <View>
                 {this.renderCards()}
-            </Animated.View>
+            </View>
         );
     }
 }
